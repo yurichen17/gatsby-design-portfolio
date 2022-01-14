fc7a7e6dbf4be0e0b65ff6bc6c031c92cd2397d6
yurichen17 <68716357+yurichen17@users.noreply.github.com>
2022-01-14 00:21:14 -0500

    testing meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://yuridesignportfolio.gatsbyjs.io",
     title: "My Design Portfolio",
     image: "/lily.png",
   },
